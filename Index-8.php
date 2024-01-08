93f105a2026bfdb183e99e224f55afea4b3f2a47
De-Vid <139108709+De-Vid@users.noreply.github.com>
2024-01-08 22:33:35 +0700

    Add files via upload

---
+++ Index-8.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $s = 1000;
+        var_dump($s);
+    ?>
+
+</body>
+</html>
